c320c50f5a5c562a13fa7a77b8da46c4e65e4f41
Enis Soztutar <enis@apache.org>
2012-06-19 16:59:21 -0700

    Split Generator into two MR jobs, so that MR tasks are idempotent, and task failures does not cause Verify.verify() to fail

--- src/main/java/goraci/Loop.java
+++ src/main/java/goraci/Loop.java
@@ -28,41 +28,45 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-/** 
+/**
  * Executes Generate and Verify in a loop. Data is not cleaned between runs, so each iteration
  * adds more data.
  */
 public class Loop extends Configured implements Tool {
 
-  private static final Log LOG = LogFactory.getLog(Loop.class); 
-  
-  protected void runGenerator(int numMappers, long numNodes) throws Exception {
+  private static final Log LOG = LogFactory.getLog(Loop.class);
+
+  protected void runGenerator(int numMappers, long numNodes, String outputDir) throws Exception {
+    Path outputPath = new Path(outputDir);
+    UUID uuid = UUID.randomUUID(); //create a random UUID.
+    Path generatorOutput = new Path(outputPath, uuid.toString());
+
     Generator generator = new Generator();
     generator.setConf(getConf());
-    int retCode = generator.run(numMappers, numNodes);
-    
+    int retCode = generator.run(numMappers, numNodes, generatorOutput);
+
     if (retCode > 0) {
       throw new RuntimeException("Generator failed with return code: " + retCode);
     }
   }
-  
+
   protected void runVerify(String outputDir, int numReducers, long expectedNumNodes) throws Exception {
     Path outputPath = new Path(outputDir);
     UUID uuid = UUID.randomUUID(); //create a random UUID.
     Path iterationOutput = new Path(outputPath, uuid.toString());
-    
+
     Verify verify = new Verify();
     verify.setConf(getConf());
     int retCode = verify.run(iterationOutput, numReducers);
     if (retCode > 0) {
       throw new RuntimeException("Verify.run failed with return code: " + retCode);
     }
-    
+
     boolean verifySuccess = verify.verify(expectedNumNodes);
     if (!verifySuccess) {
       throw new RuntimeException("Verify.verify failed");
     }
-    
+
     LOG.info("Verify finished with succees. Total nodes=" + expectedNumNodes);
   }
 
@@ -72,35 +76,35 @@ public class Loop extends Configured implements Tool {
       System.err.println("Usage: Loop <num iterations> <num mappers> <num nodes per mapper> <output dir> <num reducers>");
       return 1;
     }
-    
+
     LOG.info("Running Loop with args:" + Arrays.deepToString(args));
-    
+
     int numIterations = Integer.parseInt(args[0]);
     int numMappers = Integer.parseInt(args[1]);
     long numNodes = Long.parseLong(args[2]);
     String outputDir = args[3];
     int numReducers = Integer.parseInt(args[4]);
-    
+
     long expectedNumNodes = 0;
-    
+
     if (numIterations < 0) {
       numIterations = Integer.MAX_VALUE; //run indefinitely (kind of)
     }
-    
+
     for (int i=0; i < numIterations; i++) {
       LOG.info("Starting iteration = " + i);
-      runGenerator(numMappers, numNodes);
+      runGenerator(numMappers, numNodes, outputDir);
       expectedNumNodes += numMappers * numNodes;
-      
+
       runVerify(outputDir, numReducers, expectedNumNodes);
     }
-    
+
     return 0;
   }
-  
+
   public static void main(String[] args) throws Exception {
     int ret = ToolRunner.run(new Loop(), args);
     System.exit(ret);
   }
-  
+
 }
